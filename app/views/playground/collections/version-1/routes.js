f3b6e8fd36ea8b06228e09d573975bb4b8e4a048
Simon Whatley <simonwhatley@gmail.com>
2018-01-22 12:46:52 +0000

    Updated Collections v1

--- app/views/playground/collections/version-1/routes.js
+++ app/views/playground/collections/version-1/routes.js
@@ -1,57 +1,63 @@
 const express = require('express')
-const router = express.Router()
-
-//const session = require('express-session')
-
-//const utils = require('../../../utils')
-
-const offences = require('./data.json')
-
-console.log(offences)
+const router = new express.Router()
 
 router.use('/', (req, res, next) => {
   req.section = req.originalUrl.split('/')[1]
   req.feature = req.originalUrl.split('/')[2]
   req.version = req.originalUrl.split('/')[3]
   next()
-});
+})
 
 // Route index page
 router.get('/', function (req, res) {
-  // res.render('class')
-  res.redirect(`/${req.section}/${req.feature}/${req.version}/class`)
-});
+  res.redirect(`/${req.section}/${req.feature}/${req.version}/browse`)
+})
 
+// Route for classes
+router.get('/browse', function (req, res) {
 
-router.get('./class', function (req, res) {
+  res.render(`${req.section}/${req.feature}/${req.version}/class`,
+    {
+      base: req.baseUrl
+    })
 
-  res.render(`/${req.section}/${req.feature}/${req.version}/class`,
-  	{
-  		'previous_url': ''
-  	});
-});
+})
 
 // Route for bands
-// /:classId([0-9]+)/
+router.get('/browse/:class([0-9]+)', function (req, res) {
 
-router.get('./band', function (req, res) {
+  res.render(`${req.section}/${req.feature}/${req.version}/band`,
+    {
+      base: req.baseUrl,
+      class: req.params.class
+    })
 
-  res.render(`/${req.section}/${req.feature}/${req.version}/band`,
-  	{
-  		'previous_url': './class'
-  	});
-});
+})
 
 // Route for categories
-// /:classId([0-9]+)/:bandId([0-9]+)/
+router.get('/browse/:class([0-9]+)/:band([0-9]+)', function (req, res) {
 
-router.get('./category', function (req, res) {
+  res.render(`${req.section}/${req.feature}/${req.version}/category`,
+    {
+      base: req.baseUrl,
+      class: req.params.class,
+      band: req.params.band
+    })
 
-  res.render(`/${req.section}/${req.feature}/${req.version}/category`,
-  	{
-  		'previous_url': './band'
-  	});
-});
+})
+
+// Route for categories
+router.get('/detail/:class([0-9]+)/:band([0-9]+)/:category([0-9]+)', function (req, res) {
+
+  res.render(`${req.section}/${req.feature}/${req.version}/detail`,
+    {
+      base: req.baseUrl,
+      class: req.params.class,
+      band: req.params.band,
+      category: req.params.category
+    })
+
+})
 
 // Add your routes above this line
 module.exports = router
