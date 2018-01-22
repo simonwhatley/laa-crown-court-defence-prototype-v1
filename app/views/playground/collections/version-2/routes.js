f555c6c02fbf2664fe5fc10fc79278c507bf86ce
Simon Whatley <simonwhatley@gmail.com>
2018-01-22 21:09:54 +0000

    Updates to Collections v2

--- app/views/playground/collections/version-2/routes.js
+++ app/views/playground/collections/version-2/routes.js
@@ -3,17 +3,6 @@ const router = new express.Router()
 
 const utils = require('./utils')
 
-//const offences = require('./data.json')
-
-// var bands = offences.classes.filter(function (el) {
-//   return (el.code === 1)
-// })
-
-// console.log(bands)
-// console.log(utils.getClass(offences, 1))
-// console.log(utils.getClass(1))
-// console.log(utils.getBands(1))
-
 router.use('/', (req, res, next) => {
   req.section = req.originalUrl.split('/')[1]
   req.feature = req.originalUrl.split('/')[2]
@@ -44,8 +33,7 @@ router.get('/browse/:class([0-9]+)', function (req, res) {
     {
       base: req.baseUrl,
       classes: utils.getClasses(),
-      bands: utils.getBands(3), // replace hard coded numeral with req.params.class 
-      class: req.params.class // replace with the class ID from the object
+      class: utils.getClass(req.params.class)
     })
 
 })
@@ -53,14 +41,14 @@ router.get('/browse/:class([0-9]+)', function (req, res) {
 // Route for categories
 router.get('/browse/:class([0-9]+)/:band([0-9]+)', function (req, res) {
 
+  console.log(utils.getBand(req.params.class, req.params.band))
+
   res.render(`${req.section}/${req.feature}/${req.version}/category`,
     {
       base: req.baseUrl,
       classes: utils.getClasses(),
-      bands: utils.getBands(3),
-      // categories: utils.getCategories(3,3),
-      class: req.params.class,
-      band: req.params.band
+      class: utils.getClass(req.params.class),
+      band: utils.getBand(req.params.class, req.params.band)
     })
 
 })
