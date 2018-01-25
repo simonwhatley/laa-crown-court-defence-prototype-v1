9545ae359c7122a048141e3c04d9ae9ccc9eeb18
Simon Whatley <simonwhatley@gmail.com>
2018-01-25 11:16:38 +0000

    Updates to search-filter version 2
    Using scheme 10 and show/hide buttons on hover

---
+++ app/views/playground/search-filter/version-2/utils.js
@@ -0,0 +1,46 @@
+var offences = require('./data/scheme_10_offences.json')
+
+module.exports = {
+
+	getOffences: function() {
+		return offences
+	},
+
+	getOffencesByClassId: function(class_id) {
+		
+		var classObj = this.getOffences().filter(function (el) {
+  			 return (el.class_id == class_id)
+		})
+
+		return classObj[0]
+	},
+
+	getOffencesByBandId: function(band_id) {
+		
+		var classObj = this.getOffences().filter(function (el) {
+  			 return (el.band_id == band_id)
+		})
+
+		return classObj[0]
+	},
+
+	getOffencesByCategoryId: function(category_id) {
+		
+		var classObj = this.getOffences().filter(function (el) {
+  			 return (el.category_id == category_id)
+		})
+
+		return classObj[0]
+	},
+
+	getOffencesByActId: function(act_id) {
+		
+		var classObj = this.getOffences().filter(function (el) {
+  			 return (el.act_id == act_id)
+		})
+
+		return classObj[0]
+	}
+
+	
+}
