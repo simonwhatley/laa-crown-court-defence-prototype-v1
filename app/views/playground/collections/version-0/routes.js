d3454e35a0dc1b771e8cea0366711b1e0fbed58e
Simon Whatley <simonwhatley@gmail.com>
2018-01-25 15:11:42 +0000

    Updated collections

--- app/views/playground/collections/version-0/routes.js
+++ app/views/playground/collections/version-0/routes.js
@@ -1,2 +1,17 @@
 const express = require('express')
-const router = express.Router()
+const router = new express.Router()
+
+router.use('/', (req, res, next) => {
+  req.section = req.originalUrl.split('/')[1]
+  req.feature = req.originalUrl.split('/')[2]
+  req.version = req.originalUrl.split('/')[3]
+  next()
+})
+
+// Route index page
+router.get('/', function (req, res) {
+  res.redirect(`/${req.section}/${req.feature}/${req.version}/class`)
+})
+
+// Add your routes above this line
+module.exports = router
