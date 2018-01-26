d6487ee30176081f382c85311e109a0c5e8499e7
Simon Whatley <simonwhatley@gmail.com>
2018-01-26 16:45:00 +0000

    Updates to search-filter version 3
    Search and browse functionality

--- app/views/playground/search-filter/version-3/routes.js
+++ app/views/playground/search-filter/version-3/routes.js
@@ -12,16 +12,52 @@ router.use('/', (req, res, next) => {
 
 // Route index page
 router.get('/', function (req, res) {
-  res.redirect(`/${req.section}/${req.feature}/${req.version}/filter`)
+  res.redirect(`/${req.section}/${req.feature}/${req.version}/search`)
 })
 
-// Route for classes
-router.get('/filter', function (req, res) {
+// Route for search
+router.get('/search', function (req, res) {
 
   res.render(`${req.section}/${req.feature}/${req.version}/offences`,
     {
       base: req.baseUrl,
       offences: utils.getOffences()
+    })
+
+})
+
+// Route for class detail
+router.get('/detail/:class([0-9]+)', function (req, res) {
+
+  res.render(`${req.section}/${req.feature}/${req.version}/detail`,
+    {
+      base: req.baseUrl,
+      type: "class",
+      offences: utils.getOffencesByClassId(req.params.class)
+    })
+
+})
+
+// Route for band detail
+router.get('/detail/:class([0-9]+)/:band([0-9]+)/', function (req, res) {
+
+  res.render(`${req.section}/${req.feature}/${req.version}/detail`,
+    {
+      base: req.baseUrl,
+      type: "band",
+      offences: utils.getOffencesByBandId(req.params.class, req.params.band)
+    })
+
+})
+
+// Route for act detail
+router.get('/detail/act/:act([0-9]+)', function (req, res) {
+
+  res.render(`${req.section}/${req.feature}/${req.version}/detail`,
+    {
+      base: req.baseUrl,
+      type: "act",
+      offences: utils.getOffencesByActId(req.params.act)
     })
 
 })
