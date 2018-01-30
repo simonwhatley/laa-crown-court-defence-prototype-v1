43334e9c7912a6d71d0f0b94f6a2d6bebdbad83d
Simon Whatley <simonwhatley@gmail.com>
2018-01-30 17:32:27 +0000

    Updates to dependent dropdown version-0

--- app/views/playground/dependent-dropdowns/version-0/routes.js
+++ app/views/playground/dependent-dropdowns/version-0/routes.js
@@ -22,10 +22,45 @@ router.get('/search', function (req, res) {
   res.render(`${req.section}/${req.feature}/${req.version}/offences`,
     {
       base: req.baseUrl,
-      offences: utils.getOffences()
+      classes: utils.getOffences().classes
     })
 
 })
+
+
+
+router.get('/data/categories', function (req, res) {
+
+  console.log(req.session.data)
+  // console.log(req.session.data.depdrop_all_params['offence-class'])
+
+  // console.log(utils.getOffenceClass(req.session.data.depdrop_parents[0]).categories)
+
+  var data = "{ output: " + JSON.stringify(utils.getOffenceClass(req.session.data.class[0]).categories) + ", selected: '' }"
+
+  console.log(data)
+
+  res.json(data)
+
+})
+
+router.get('/data/categories/:class([A-K])', function (req, res) {
+  
+  // var data = "{ output: " + utils.getOffenceClass(req.params.class).categories + ", selected: '' }"
+  var data = "{ output: " + JSON.stringify(utils.getOffenceClass(req.params.class).categories) + ", selected: '' }"
+
+  res.send(data)
+
+  // res.json(utils.getOffenceClass(req.params.class))
+  // res.json(utils.getOffenceClass(req.params.class).categories)
+
+})
+
+// router.get('/data/:class([A-K])/:category([0-9]+)', function (req, res) {
+
+//   res.json(utils.getOffenceCategory(req.params.class, req.params.category))
+
+// })
 
 // Add your routes above this line
 module.exports = router
--- app/views/playground/dependent-dropdowns/version-0/utils.js
+++ app/views/playground/dependent-dropdowns/version-0/utils.js
@@ -4,6 +4,24 @@ module.exports = {
 
 	getOffences: function() {
 		return offences
+	},
+
+	getOffenceClass: function(class_id) {
+		
+		var classObj = this.getOffences().classes.filter(function (el) {
+  			 return (el.id == class_id)
+		})
+
+		return classObj[0]
+	},
+
+	getOffenceCategory: function(class_id, category_id) {
+		
+		var classObj = this.getOffenceClass(class_id).categories.filter(function (el) {
+  			 return (el.id == category_id)
+		})
+
+		return classObj[0]
 	}
 	
 }
