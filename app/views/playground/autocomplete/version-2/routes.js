57c306285994b01603cd820dc552bf6d24290730
Simon Whatley <simonwhatley@gmail.com>
2018-02-01 15:27:01 +0000

    Autocomplete version-2 using scheme 9 offence data

---
+++ app/views/playground/autocomplete/version-2/routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = new express.Router()
+
+const utils = require('./utils')
+
+router.use('/', (req, res, next) => {
+  req.section = req.originalUrl.split('/')[1]
+  req.feature = req.originalUrl.split('/')[2]
+  req.version = req.originalUrl.split('/')[3]
+  next()
+})
+
+// Route index page
+router.get('/', function (req, res) {
+  req.session.destroy()
+  res.redirect(`/${req.section}/${req.feature}/${req.version}/find`)
+})
+
+// Route for offence lookup
+router.get('/find', function (req, res) {
+
+console.log(utils.getOffences())
+
+  res.render(`${req.section}/${req.feature}/${req.version}/find`,
+    {
+      base: req.baseUrl,
+      offences: utils.getOffences()
+    })
+
+})
+
+// Add your routes above this line
+module.exports = router
---
+++ app/views/playground/autocomplete/version-2/scripts/cccd_offence_category_autocomplete.js
@@ -0,0 +1,90 @@
+var cccdCaseTypesAutocomplete = function(options) {
+
+  var sourceSelect = function(query, callback) {
+
+    var optionsWithAValue = [].filter.call(options.selectElement.options, function(option) {
+      return option.value != ''
+    })
+
+    var orgs = optionsWithAValue.map(function(select) {
+
+      var dataAbbreviations = select.getAttribute('data-abbreviations');
+      dataAbbreviations = dataAbbreviations ? dataAbbreviations.split('|') : []
+
+      var dataOtherNames = select.getAttribute('data-other-names');
+      dataOtherNames = dataOtherNames ? dataOtherNames.split('|') : []
+
+      return {
+        'name': select.label,
+        'abbreviations': dataAbbreviations,
+        'other_names': dataOtherNames
+      }
+    })
+
+    var regexes = query.trim().split(/\s+/).map(function(word) {
+      return new RegExp('\\b' + word, 'i')
+    })
+
+    var matches = orgs.map(function(offence) {
+
+      var allNames = [offence.name]
+        .concat(offence.other_names)
+        .concat(offence.abbreviations)
+        .filter(function(name) { return name })
+
+      offence['resultPosition'] = null
+
+
+      for (var i = 0; i < allNames.length; i++) {
+
+        var matches = regexes.reduce(function(acc, regex) {
+
+          matchPosition = allNames[i].search(regex)
+          if (matchPosition > -1) {
+            acc.count += 1
+
+            if (acc.lowestPosition == -1 || matchPosition < acc.lowestPosition) {
+              acc.lowestPosition = matchPosition
+            }
+          }
+
+          return acc;
+
+        }, {'count': 0, 'lowestPosition': -1})
+
+
+        if (matches.count == regexes.length && (offence['resultPosition'] == null || matches.lowestPosition < offence['resultPosition'])) {
+          offence['resultPosition'] = matches.lowestPosition
+        }
+      }
+
+      return offence
+
+    })
+
+    var filteredMatches = matches.filter(function(offence) {
+      return (offence['resultPosition'] != null )
+    })
+
+    var sortedFilteredMatches = filteredMatches.sort(function(offenceA, offenceB) {
+
+      if (offenceA['resultPosition'] < offenceB['resultPosition'] ) {
+        return -1
+      } else if (offenceA['resultPosition'] > offenceB['resultPosition'] ) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+    var results = sortedFilteredMatches.map(function(offence) { return offence['name'] })
+
+    return callback(results)
+  }
+
+
+  options.source = sourceSelect
+
+  accessibleAutocomplete.enhanceSelectElement(options)
+
+}
