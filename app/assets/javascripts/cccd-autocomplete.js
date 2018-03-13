9277d34f94f7aae41145530310734fb9eba1124e
Simon Whatley <simonwhatley@gmail.com>
2018-03-13 12:59:45 +0000

    Replace special characters in regex

--- app/assets/javascripts/cccd-autocomplete.js
+++ app/assets/javascripts/cccd-autocomplete.js
@@ -4,87 +4,90 @@ var autocomplete = function(options) {
 
     var optionsWithAValue = [].filter.call(options.selectElement.options, function(option) {
       return option.value != ''
-    })
+    });
 
     var things = optionsWithAValue.map(function(select) {
 
       var dataAbbreviations = select.getAttribute('data-abbreviations');
-      dataAbbreviations = dataAbbreviations ? dataAbbreviations.split('|') : []
+      dataAbbreviations = dataAbbreviations ? dataAbbreviations.split('|') : [];
 
       var dataOtherNames = select.getAttribute('data-other-names');
-      dataOtherNames = dataOtherNames ? dataOtherNames.split('|') : []
+      dataOtherNames = dataOtherNames ? dataOtherNames.split('|') : [];
 
       return {
         'name': select.label,
         'abbreviations': dataAbbreviations,
         'other_names': dataOtherNames
       }
-    })
+    });
 
     var regexes = query.trim().split(/\s+/).map(function(word) {
-      return new RegExp('\\b' + word, 'i')
-    })
+      var pattern = '\\b' + word.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+      return new RegExp(pattern, 'i');
+    });
 
     var matches = things.map(function(thing) {
 
       var allNames = [thing.name]
         .concat(thing.other_names)
         .concat(thing.abbreviations)
-        .filter(function(name) { return name })
+        .filter(function(name) {
+          return name;
+        });
 
-      thing['resultPosition'] = null
-
+      thing['resultPosition'] = null;
 
       for (var i = 0; i < allNames.length; i++) {
 
         var matches = regexes.reduce(function(acc, regex) {
 
-          matchPosition = allNames[i].search(regex)
+          matchPosition = allNames[i].search(regex);
           if (matchPosition > -1) {
-            acc.count += 1
+            acc.count += 1;
 
             if (acc.lowestPosition == -1 || matchPosition < acc.lowestPosition) {
-              acc.lowestPosition = matchPosition
+              acc.lowestPosition = matchPosition;
             }
           }
 
           return acc;
 
-        }, {'count': 0, 'lowestPosition': -1})
+        }, {'count': 0, 'lowestPosition': -1});
 
 
         if (matches.count == regexes.length && (thing['resultPosition'] == null || matches.lowestPosition < thing['resultPosition'])) {
-          thing['resultPosition'] = matches.lowestPosition
+          thing['resultPosition'] = matches.lowestPosition;
         }
       }
 
-      return thing
+      return thing;
 
-    })
+    });
 
     var filteredMatches = matches.filter(function(thing) {
-      return (thing['resultPosition'] != null )
-    })
+      return (thing['resultPosition'] != null);
+    });
 
     var sortedFilteredMatches = filteredMatches.sort(function(thingA, thingB) {
 
       if ( thingA['resultPosition'] < thingB['resultPosition'] ) {
-        return -1
+        return -1;
       } else if ( thingA['resultPosition'] > thingB['resultPosition'] ) {
-        return 1
+        return 1;
       } else {
-        return 0
+        return 0;
       }
     })
 
-    var results = sortedFilteredMatches.map(function(thing) { return thing['name'] })
+    var results = sortedFilteredMatches.map(function(thing) {
+      return thing['name'];
+    });
 
-    return callback(results)
+    return callback(results);
   }
 
+  options.source = sourceSelect;
 
-  options.source = sourceSelect
-
-  accessibleAutocomplete.enhanceSelectElement(options)
+  accessibleAutocomplete.enhanceSelectElement(options);
 
 }
