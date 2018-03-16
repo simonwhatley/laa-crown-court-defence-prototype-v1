6231dd495ca1f3f842d312d61bf5d92de895c0cb
Simon Whatley <simonwhatley@gmail.com>
2018-03-16 22:34:24 +0000

    Updates to version-0 of prototype litigator claim flow

--- app/views/providers/version-0/routes.js
+++ app/views/providers/version-0/routes.js
@@ -6,7 +6,46 @@ const utils = require('./utils')
 router.get('/', (req, res) => {
     req.session.destroy()
     res.redirect(`/${req.feature}/${req.version}/sign-in`)
-})
+});
+
+router.get('/sign-in', function(req, res) {
+    res.render(`${req.feature}/${req.version}/sign-in`,
+        {
+            proposition_links: "hide"
+        });
+});
+
+router.get('/auth', function(req, res) {
+
+    if (utils.authenticate("provider", req.session.data.username) == "advocates") {
+
+        res.redirect(`/${req.feature}/${req.version}/advocates/`);
+
+    } else if (utils.authenticate("provider", req.session.data.username) == "litigators") {
+
+        res.redirect(`/${req.feature}/${req.version}/litigators/`);
+
+    } else {
+
+        // TODO: Check user input for errors
+
+        res.render(`${req.feature}/${req.version}/sign-in`,
+            {
+                errors: {
+                    username: "Enter a valid email address",
+                    password: "Enter a valid password"
+            }
+        });
+
+        // res.redirect('sign-in')
+    }
+
+});
+
+router.get('/sign-out', function(req, res) {
+    req.session.destroy()
+    res.redirect(`/${req.feature}/${req.version}/sign-in`);
+});
 
 // ==============================================
 // Data
@@ -15,7 +54,7 @@ router.get('/', (req, res) => {
 router.get('/data/offences/9/classes', function (req, res) {
     var data = { output: utils.getOffenceClasses(req.session.data.category[0]) , selected: req.session.data.offence_class }
     res.json(data)
-})
+});
 
 // ==============================================
 // ADVOCATES
@@ -25,13 +64,18 @@ router.get('/advocates/', function(req, res) {
     res.render(`${req.feature}/${req.version}/advocates/index`,
         {
             links: {
-                'start' : req.baseUrl + '/advocates/case-details',
+                'start' : req.baseUrl + '/advocates/start',
                 'search': req.baseUrl + '/advocates/search',
                 'outstanding': req.baseUrl + '/advocates/outstanding',
                 'authorised': req.baseUrl + '/advocates/authorised'
             },
             claims: []
         });
+});
+
+router.get('/advocates/start', function(req, res) {
+    req.session.destroy()
+    res.redirect(`/${req.feature}/${req.version}/advocates/case-details`);
 });
 
 router.get('/advocates/case-details', function(req, res) {
@@ -70,7 +114,6 @@ router.get('/advocates/check-claim', function(req, res) {
                 'next' : req.baseUrl + '/advocates/certify-claim',
                 'previous' : req.baseUrl + '/advocates/fee-details'
             },
-            fee_type: req.session.data.fee_type,
             offence: { "category_label": utils.getOffenceCategoryName(req.session.data.offence_category), "class_label": utils.getOffenceClassName(req.session.data.offence_class, req.session.data.offence_category) }
         });
 });
@@ -103,7 +146,7 @@ router.get('/litigators/', function(req, res) {
     res.render(`${req.feature}/${req.version}/litigators/index`,
         {
             links: {
-                'start' : req.baseUrl + '/litigators/bill-type',
+                'start' : req.baseUrl + '/litigators/start',
                 'search': req.baseUrl + '/litigators/search',
                 'outstanding': req.baseUrl + '/litigators/outstanding',
                 'authorised': req.baseUrl + '/litigators/authorised'
@@ -112,8 +155,13 @@ router.get('/litigators/', function(req, res) {
         });
 });
 
+router.get('/advocates/start', function(req, res) {
+    req.session.destroy()
+    res.redirect(`/${req.feature}/${req.version}/advocates/bill-type`);
+});
+
 router.get('/litigators/bill-type', function(req, res) {
-    
+
     res.render(`${req.feature}/${req.version}/litigators/bill-type`,
         {
             links: {
@@ -127,13 +175,13 @@ router.get('/litigators/bill-type', function(req, res) {
 router.get('/litigators/details', function(req, res) {
 
     if (req.session.data.bill_type == "litigator_transfer") {
-        
+
         res.redirect(`/${req.feature}/${req.version}/litigators/transfer-details`)
 
     } else {
-        
+
         res.redirect(`/${req.feature}/${req.version}/litigators/case-details`)
-            
+
     }
 
 });
@@ -146,7 +194,7 @@ router.get('/litigators/transfer-details', function(req, res) {
                 'previous' : req.baseUrl + '/litigators/bill-type',
                 'home' : req.baseUrl + '/litigators/'
             }
-            
+
         });
 });
 
@@ -157,7 +205,7 @@ router.get('/litigators/case-details', function(req, res) {
         var previousUrl = req.baseUrl + '/litigators/transfer-details'
 
     } else {
-        
+
         var previousUrl = req.baseUrl + '/litigators/bill-type'
 
     }
@@ -185,6 +233,7 @@ router.get('/litigators/fee-details', function(req, res) {
             fee_type: utils.getFeeTypeForCaseType(req.session.data.case_type),
             fixed_fees: utils.getFixedFees('lgfs','9'),
             misc_fees: utils.getMiscellaneousFees('lgfs','9'),
+            disbursements: utils.getDisbursements('lgfs','9'),
             travel_types: utils.getTravelTypes('lgfs'),
             travel_reasons: utils.getTravelReasons()
         });
@@ -196,7 +245,8 @@ router.get('/litigators/check-claim', function(req, res) {
             links: {
                 'next' : req.baseUrl + '/litigators/certify-claim',
                 'previous' : req.baseUrl + '/litigators/fee-details'
-            }
+            },
+            offence: { "category_label": utils.getOffenceCategoryName(req.session.data.offence_category), "class_label": utils.getOffenceClassName(req.session.data.offence_class, req.session.data.offence_category) }
         });
 });
 
