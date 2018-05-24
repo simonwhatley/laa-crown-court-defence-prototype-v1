9ee1bad0d287c5d552eb740f7fa435abd035a7f6
Simon Whatley <simonwhatley@gmail.com>
2018-05-24 14:29:36 +0100

    Admins version 1 edit providers

--- app/views/admins/version-1/routes.js
+++ app/views/admins/version-1/routes.js
@@ -1,13 +1,272 @@
 const express = require('express')
 const router = express.Router()
 
+const utils = require('./utils');
+
+// ==============================================
+// COMMON
+// ==============================================
+
+// Route to the start (sign in) page
 router.get('/', (req, res) => {
-  res.redirect(`/${req.feature}/${req.version}/sign-in`)
-})
+  res.redirect(`/${req.feature}/${req.version}/sign-in`);
+});
 
-// Add your routes here - above the module.exports line
+router.get('/sign-in', function(req, res) {
+    res.render(`${req.feature}/${req.version}/sign-in`,
+        {
+            proposition_links: "hide"
+        });
+});
 
+router.get('/auth', function(req, res) {
 
+	res.redirect(`/${req.feature}/${req.version}/home`);
+
+    
+
+});
+
+router.get('/sign-out', function(req, res) {
+    req.session.destroy()
+    res.redirect(`/${req.feature}/${req.version}/sign-in`);
+});
+
+// ==============================================
+// HOME
+// ==============================================
+
+router.get('/home', function(req, res) {
+    res.render(`${req.feature}/${req.version}/home`,
+        {
+            links: {
+                
+            }
+        });
+});
+
+// ==============================================
+// PROVIDERS
+// ==============================================
+
+// Route for provider list
+router.get('/providers', function (req, res) {
+
+  // Total number of providers
+  var count = utils.getProviderCount();
+
+  // Prevent users putting in a limit not in the pre-defined set: 10, 25, 50, 100
+  var limit = 100;
+  if ([10,25,50,100].indexOf(parseInt(req.query.limit)) !== -1) {
+    var limit = (req.query.limit) ? parseInt(req.query.limit) : 100;
+  }
+  
+  var sort_by = (req.query.sort) ? req.query.sort : 'name';
+  var sort_order = (req.query.order) ? req.query.order : 'asc';
+
+  // Current page
+  var page = (req.query.page) ? parseInt(req.query.page) : 1;
+
+  // Total number of pages
+  var page_count = Math.ceil(count / limit);
+
+  var start_page = 1;
+  var end_page = 5;
+
+  // First five pages
+  if (page > 3) {
+  	start_page = page - 2;
+  	end_page = page + 2;
+  }
+
+  // Last five pages
+  if (page > (page_count - 3)) {
+  	start_page = page_count - 4;
+  	end_page = page_count;
+  }
+
+  var prev_page = page - 1;
+  var next_page = page + 1;
+  
+  var start_item = (page == 1) ? page : ((page*limit)-limit)+1;
+  var end_item = (page == 1) ? (page*limit) : ((start_item+limit)-1);
+
+  res.render(`${req.feature}/${req.version}/providers/list`,
+    {
+      links: {
+      	  provider: {
+        	'add' : req.baseUrl + '/provider/add',
+        	'view' : req.baseUrl + '/provider',
+      	  },
+      	  user: {
+      	  	'add' : req.baseUrl + '/provider/' + req.params.provider_id + '/user/add',
+      	  	'edit' : req.baseUrl + '/provider/' + req.params.provider_id + '/user/'
+      	  },
+      	  'list': req.baseUrl + '/providers',
+          'back': req.baseUrl + '/home'
+      },
+      pagination: {
+        total_count: count,
+        start_item: start_item,
+        end_item: end_item,
+        page_count: page_count,
+        current_page: page,
+        start_page: start_page,
+        end_page: end_page,
+        prev_page: prev_page,
+        next_page: next_page,
+        limit: limit,
+        sort_by: sort_by,
+        sort_order: sort_order
+      },
+      providers: utils.getProviders(sort_by,sort_order,limit,page)
+    });
+
+});
+
+// Route for single provider
+router.get('/provider/:provider_id([0-9]+)/', function (req, res) {
+
+  if (typeof(req.session.data.provider) === 'undefined') {
+  	var provider = utils.getProvider(req.params.provider_id);
+  }
+  else {
+
+  	if (parseInt(req.params.provider_id) === parseInt(req.session.data.provider.id)) {
+  		var provider = req.session.data.provider;
+  	}
+  	else {
+  		var provider = utils.getProvider(req.params.provider_id);
+  	}
+  }
+
+  res.render(`${req.feature}/${req.version}/providers/view`,
+    {
+      links: {
+      	  provider: {
+        	'edit' : req.baseUrl + '/provider/' + req.params.provider_id + '/edit',
+        	'delete' : req.baseUrl + '/provider/' + req.params.provider_id + '/delete'
+      	  },
+      	  user: {
+      	  	'add' : req.baseUrl + '/provider/' + req.params.provider_id + '/user/add',
+      	  	'edit' : req.baseUrl + '/provider/' + req.params.provider_id + '/user/'
+      	  },
+          'back': req.baseUrl + '/providers'
+      },
+      provider: provider
+    });
+
+});
+
+// Route for single provider
+router.get('/provider/add', function (req, res) {
+
+  res.render(`${req.feature}/${req.version}/providers/edit`,
+    {
+      links: {
+          'save' : req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'cancel': req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'back': req.baseUrl + '/providers'
+      },
+      provider: utils.getProvider(req.params.provider_id)
+    });
+
+});
+
+// Route for single provider
+router.get('/provider/:provider_id([0-9]+)/edit', function (req, res) {
+  
+  if (typeof(req.session.data.provider) === 'undefined') {
+  	var provider = utils.getProvider(req.params.provider_id);
+  }
+  else {
+
+  	if (parseInt(req.params.provider_id) === parseInt(req.session.data.provider.id)) {
+  		var provider = req.session.data.provider;
+  	}
+  	else {
+  		var provider = utils.getProvider(req.params.provider_id);
+  	}
+  }
+
+  res.render(`${req.feature}/${req.version}/providers/edit`,
+    {
+      links: {
+          'save' : req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'cancel': req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'back': req.baseUrl + '/provider/' + req.params.provider_id + '/'
+      },
+      provider: provider
+    });
+
+});
+
+// ==============================================
+// PROVIDER USERS
+// ==============================================
+
+// Route for single provider
+router.get('/provider/:provider_id([0-9]+)/users', function (req, res) {
+
+  res.render(`${req.feature}/${req.version}/providers/view`,
+    {
+      links: {
+          'edit' : req.baseUrl + '/provider/' + req.params.provider_id + '/edit',
+          'back': req.baseUrl + '/providers'
+      },
+      users: utils.getProviderUsers(req.params.provider_id)
+    });
+
+});
+
+// Route for single user
+router.get('/provider/:provider_id([0-9]+)/user/:user_id([0-9]+)/view', function (req, res) {
+
+  req.session.data.user = utils.getUser(req.params.user_id)
+
+  res.render(`${req.feature}/${req.version}/providers/users/view`,
+    {
+      links: {
+          'save' : req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'cancel': req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'back': req.baseUrl + '/provider/' + req.params.provider_id + '/'
+      },
+      user: req.session.data.user
+    });
+
+});
+
+// Route for single user
+router.get('/provider/:provider_id([0-9]+)/user/add', function (req, res) {
+
+  res.render(`${req.feature}/${req.version}/providers/users/edit`,
+    {
+      links: {
+          'save' : req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'cancel': req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'back': req.baseUrl + '/provider/' + req.params.provider_id + '/'
+      }
+      // ,
+      // provider: utils.getProvider(req.params.provider_id)
+    });
+
+});
+
+// Route for single user
+router.get('/provider/:provider_id([0-9]+)/user/:user_id([0-9]+)/edit', function (req, res) {
+
+  res.render(`${req.feature}/${req.version}/providers/users/edit`,
+    {
+      links: {
+          'save' : req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'cancel': req.baseUrl + '/provider/' + req.params.provider_id + '/',
+          'back': req.baseUrl + '/provider/' + req.params.provider_id + '/'
+      }
+      // ,
+      // user: utils.getUser(req.params.user_id)
+    });
+
+});
 
 // Add your routes above this line
 
---
+++ app/views/admins/version-1/utils.js
@@ -0,0 +1,100 @@
+var providers = require('./data/providers.json');
+
+module.exports = {
+
+	// See: Stackoverflow
+	// Q: https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
+	// A: https://stackoverflow.com/a/979325
+	sortBy: function(field, reverse, primer){
+
+	   var key = primer ?
+	       function(x) {return primer(x[field])} :
+	       function(x) {return x[field]};
+
+	   reverse = !reverse ? 1 : -1;
+
+	   return function (a, b) {
+	       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+	     }
+	},
+
+	getProviders: function(sort_by,sort_order,limit,page) {
+
+		var data = [];
+
+		if (!limit) limit = 100;
+
+		if (!page) page = 1;
+
+		var start = (page == 1) ? 0 : ((page * limit)-limit);
+		var end = (page * limit);
+
+		var order = (sort_order == 'desc') ? true : false;
+
+		switch(sort_by) {
+			case 'id':
+				data = providers.sort(this.sortBy('id', order, parseInt));
+				break;
+			case 'type':
+				data = providers.sort(this.sortBy('provider_type', order, function(a) {
+					return a.toUpperCase();
+				}));
+				break;
+			case 'scheme':
+				// TODO
+
+				break;
+			case 'status':
+				// TODO sort by status and provider name...remove items that aren't the correct status???
+				data = providers.sort(this.sortBy('account_status', order, function(a) {
+					return a.toUpperCase();
+				}));
+				break;
+			case 'vat':
+				// TODO
+
+				break;
+			default:
+				data = providers.sort(this.sortBy('provider_name', order, function(a) {
+					return a.toUpperCase();
+				}));
+				break;
+		}
+
+		return data.slice(start,end);
+
+	},
+
+	getProvider: function(provider_id) {
+
+		if (!provider_id) return null
+
+		var providerObj = providers.filter( function(obj) {
+			return (obj.id == provider_id);
+		});
+
+		return providerObj[0];
+
+	},
+
+	getProviderCount: function() {
+		return parseInt(providers.length);
+	},
+
+	getProviderUsers: function(provider_id) {
+
+		if (!provider_id) return null
+
+		var userObj = users.filter( function(obj) {
+			return (obj.provider_id == provider_id);
+		});
+
+		return userObj[0];
+
+	},
+
+	getUser: function(user_id) {
+		
+	}
+
+}
