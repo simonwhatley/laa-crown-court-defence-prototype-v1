d9516c6e47b685bf2b41464ac6a963ab18997962
Simon Whatley <simonwhatley@gmail.com>
2018-05-30 12:04:44 +0100

    Caseworkers version 2 continue building claim assessment view
    Trial, retrial, cracked trial and cracked before retrial details
    Defendants
    Offence details
    Miscellaneous fees
    Warrant details
    Travel expenses
    Supporting evidence
    Additional information

--- app/views/caseworkers/version-2/utils.js
+++ app/views/caseworkers/version-2/utils.js
@@ -81,47 +81,124 @@ module.exports = {
 
 		// return claimObj[0];
 
-		var data =  [
-    {
-      "id": 12345,
-      "case_number": "T20180101",
-      "court": "Cardiff",
-      "fee_scheme": "agfs",
-      "fee_scheme_version": 9,
-      "bill_type": "advocate_final",
-      "case_type": "trial",
-      "fee_type": "graduated",
-      "date_submitted": "2018-02-01T10:30:00.000Z",
-      "status": "allocated",
-      "reference_number": "HR/PS/72852849",
-      "total_amount": 99999,
-      "defendants": [
-        {
-          "urn": 12345,
-          "first_name": "Philippine",
-          "last_name": "Struttman",
-          "date_of_birth": "1973-06-05T00:00:00.000Z",
-          "judicial_apportionment": "yes",
-          "representation_orders": [
-            {
-              "maat_reference": 72852849,
-              "representation_order_date": "2017-09-24T00:00:00.000Z"
-            }
-          ]
-        }
-      ],
-      "certification": {
-        "certified_by": "Rufus Titterington",
-        "certified_date": "2018-02-01T10:30:00.000Z",
-        "reason": "I attended the main hearing (1st day of trial)"
-      },
-      "instructed_advocate": {
-        "code": "001PY",
-        "first_name": "Horace",
-        "last_name": "Rumpole"
-      }
-    }
-  ];
+		var data =  [{
+					    "id": 12345,
+					    "case_number": "T20180101",
+					    "court": "Cardiff",
+					    "fee_scheme": "agfs",
+					    "fee_scheme_version": 9,
+					    "bill_type": "advocate_final",
+					    "case_type": "trial",
+					    // Trial
+					    "first_day_of_trial": "2018-01-02T00:00:00.000Z",
+					    "trial_concluded_on": "2018-01-06T00:00:00.000Z",
+					    "estimated_trial_length": 5,
+					    "actual_trial_length": 5,
+					    // Retrial
+					    // "first_day_of_retrial": "2018-02-02T00:00:00.000Z",
+					    // "retrial_concluded_on": "2018-04-06T00:00:00.000Z",
+					    // "estimated_retrial_length": 2,
+					    // "actual_retrial_length": 3,
+					    // "apply_reduced_rate_retrial": "yes",
+					    // Cracked trial
+					    // "notice_of_fixed_warned_trial_issued": "2018-02-02T00:00:00.000Z",
+					    // "fixed_warned_trial": "2018-02-03T00:00:00.000Z",
+					    // "case_cracked_on": "2018-02-04T00:00:00.000Z",
+					    // "case_cracked_in": "first_third",
+					    // Cracked before retrial
+					    // "cracked_before_retrial_notice_of_fixed_warned_trial_issued": "2018-02-02T00:00:00.000Z",
+					    // "cracked_before_retrial_fixed_warned_trial": "2018-02-03T00:00:00.000Z",
+					    // "cracked_before_retrial_case_cracked_on": "2018-02-04T00:00:00.000Z",
+					    // "cracked_before_retrial_case_cracked_in": "first_third",
+					    "fee_type": "graduated",
+					    "date_submitted": "2018-02-01T10:30:00.000Z",
+					    "status": "allocated",
+					    "reference_number": "HR/PS/72852849",
+					    "total_amount": 99999,
+					    "defendants": [{
+					        "urn": 12345,
+					        "first_name": "Philippine",
+					        "last_name": "Struttman",
+					        "date_of_birth": "1973-06-05T00:00:00.000Z",
+					        "judicial_apportionment": "yes",
+					        "representation_orders": [{
+					            "maat_reference": 72852849,
+					            "representation_order_date": "2017-09-24T00:00:00.000Z"
+					            // },
+					            // {
+					            //   "maat_reference": 82852840,
+					            //   "representation_order_date": "2018-01-04T00:00:00.000Z"
+					        }]
+					    }],
+					    "certification": {
+					        "certified_by": "Rufus Titterington",
+					        "certified_date": "2018-02-01T10:30:00.000Z",
+					        "reason": "I attended the main hearing (1st day of trial)"
+					    },
+					    "instructed_advocate": {
+					        "code": "001PY",
+					        "first_name": "Horace",
+					        "last_name": "Rumpole"
+					    },
+					    "offence": {
+					        "class": "A – Homicide and related grave offences",
+					        "band": "",
+					        "category": "Murder"
+					    },
+					    "miscellaneous_fees":[
+						    {
+						    	"type": "abuse_of_process_hearings_half_day",
+						    	"date_of_hearing": "2018-01-04T00:00:00.000Z",
+						    	"net_amount": 999.99
+						    },
+						    {
+						    	"type": "abuse_of_process_hearings_whole_day",
+						    	"date_of_hearing": "2018-01-08T00:00:00.000Z",
+						    	"net_amount": 1999.99
+						    }
+					    ],
+					    "expenses": [
+					    	{
+					    		"type": "car_travel",
+					    		"reason": "court_hearing",
+					    		"destination": "Cardiff CC",
+					    		"location": "",
+					    		"hours": "",
+					    		"distance": "10",
+					    		"date_of_expense": "2018-01-09T00:00:00.000Z",
+					    		"cost_per_mile": "25",
+					    		"net_amount": 2.50,
+					    		"vat_amount": 0.50,
+					    		"additional_information": ""
+					    	},
+					    	{
+					    		"type": "car_travel",
+					    		"reason": "court_hearing",
+					    		"destination": "Cardiff CC",
+					    		"location": "",
+					    		"hours": "",
+					    		"distance": "10",
+					    		"date_of_expense": "2018-01-10T00:00:00.000Z",
+					    		"cost_per_mile": "25",
+					    		"net_amount": 2.50,
+					    		"vat_amount": 0.50,
+					    		"additional_information": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida urna ex, gravida pretium justo semper quis. Aenean vitae massa at nunc fringilla venenatis ut non velit."
+					    	}
+					    ],
+					    // "warrants": [{
+					    //     "date_issued": "2017-12-01T00:00:00.000Z",
+					    //     "date_executed": "2018-02-01T00:00:00.000Z"
+					    //     // },
+					    //     // {
+					    //     //  "date_issued": "2017-12-31T00:00:00.000Z",
+					    //     //  "date_executed": "2018-03-01T00:00:00.000Z"
+					    // }],
+					    "evidence": {
+					        "electronic_evidence": "no",
+					        "checklist": ["representation_order", "invoices"]
+					    },
+					    "additional_information": ""
+					}];
 
 		return data[0];
 
